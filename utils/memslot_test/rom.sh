2ec78362586f5f119d890fddeb27be5cfb3945eb
Sean Christopherson <seanjc@google.com>
2024-01-08 10:18:51 -0800

    bash: memslot_test: Use "roms" override with network device for PCI ROM
    Use the "roms" override to have QEMU create an option ROM for all devices,
    and point at what is hopefully the network device for the ROM.  Forcing a
    VGA device no longer works (might be a kernel "issue"?), and not requiring
    a dummy VGA device is actually desirable anyways.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- utils/memslot_test/rom.sh
+++ utils/memslot_test/rom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo 55aa > /sys/bus/pci/devices/0000:00:01.0/rom
+echo 55aa > /sys/bus/pci/devices/0000:00:02.0/rom
 
 for i in {1..2}; do
-   cat /sys/bus/pci/devices/0000:00:01.0/rom &> /dev/null
+   cat /sys/bus/pci/devices/0000:00:02.0/rom &> /dev/null
    sleep 1
 done
